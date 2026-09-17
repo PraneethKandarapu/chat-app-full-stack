7ba3443dc06c15e88ca5b5f42f9364fe158fe373
Praneeth kandarapu <praneethkandarapu@gmail.com>
2026-09-17 22:18:20 +0530

    connect registration frontend to backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,50 +1,53 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
-  const [input, setInput] = useState("");
-  const sendMessage = () => {
-    //we fetch the post route, and send body with a text message as string, the backed using express.json() will convert it to object
-    fetch("http://localhost:5000/api/messages", {
+
+  const registerUser = () => {
+    fetch("http://localhost:5000/api/auth/register", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        content: input,
+        username: username,
+        email: email,
+        password: password,
       }),
     })
       .then((response) => {
-        //converts json to js object
         return response.json();
       })
       .then((data) => {
-        setMessage(data.message);
-        setInput("");
+        setMessage(data.message || "Registration successful");
       });
   };
-  useEffect(() => {
-    fetch("http://localhost:5000/api/health")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        //"data" object received from backend after get/ request
-        setMessage(data.message);
-      });
-  }, []);
+
   return (
     <div>
-      <h1>Chat application</h1>
-
+      <h1>Create Account</h1>
       <input
         type="text"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        placeholder="Type a message"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
       />
-
-      <button onClick={sendMessage}>Send</button>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={registerUser}>Register</button>
       <p>{message}</p>
     </div>
   );
