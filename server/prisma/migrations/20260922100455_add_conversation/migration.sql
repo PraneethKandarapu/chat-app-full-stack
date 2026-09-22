3aebdf728cf039616a34dbcee010db873e42fbe5
Praneeth kandarapu <praneethkandarapu@gmail.com>
2026-09-22 15:43:41 +0530

    Add conversation database model

---
+++ server/prisma/migrations/20260922100455_add_conversation/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "Conversation" (
+    "id" SERIAL NOT NULL,
+    "user1Id" INTEGER NOT NULL,
+    "user2Id" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Conversation_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Conversation" ADD CONSTRAINT "Conversation_user1Id_fkey" FOREIGN KEY ("user1Id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Conversation" ADD CONSTRAINT "Conversation_user2Id_fkey" FOREIGN KEY ("user2Id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ server/services/conversationServices.js
@@ -0,0 +1,2 @@
+const prisma = require("../lib/prisma.js");
+const getOrCreateConversationService = async (user1Id, user2Id) => {};
